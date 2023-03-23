f1613efa22d0247da336ac4ad15143d4be7c9325
igorbaloshin <iebaloshin@gmail.com>
2023-03-23 16:53:07 +0300

    бкткемп 2

---
+++ bootcamp002/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+Console.Clear();
+
+int[] array = 1_000_000.Create()
+                .Fill(1, 10);
+// array.ConvertToStringAndPrintToTerminal();
+
+
+
+int m = 100;   // Количесто чисел последовательности
+Stopwatch sw = new Stopwatch();
+sw.Start();
+
+int max = array.BadGetSum(m);
+sw.Stop();
+
+Console.WriteLine($"BadGetSum: {max} => {sw.ElapsedMilliseconds}");
+
+sw.Reset();
+sw.Start();
+max = array.GoodGetSum(m);
+sw.Stop();
+Console.WriteLine($"GoodGetSum: {max} => {sw.ElapsedMilliseconds}");
+
+// Console.WriteLine(array.ConvertToString());
