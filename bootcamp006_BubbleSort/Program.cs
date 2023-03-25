ebac5c5b584bb7474d517273c95d7b5216f5c7e9
Igor B <iebaloshin@gmail.com>
2023-03-25 14:41:41 +0300

    буткемп 6

---
+++ bootcamp006_BubbleSort/Program.cs
@@ -0,0 +1,12 @@
+﻿using static Sorting;
+using static Infrastructure;
+Console.Clear();
+// int[] array = CreateArray(10);
+// Show(array);
+// SortSelection(array);
+// Show(array);
+
+10.CreateArray(min:1, max:10)
+  .Show()
+  .SortBubbleOptimized()
+  .Show();
