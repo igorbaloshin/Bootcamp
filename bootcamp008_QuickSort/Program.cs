f06a1a386e53dfa666064785342ef6d6c23c5673
igorbaloshin <iebaloshin@gmail.com>
2023-03-27 14:11:58 +0300

    буткемп 8

---
+++ bootcamp008_QuickSort/Program.cs
@@ -0,0 +1,11 @@
+﻿using static Sorting;
+using static System.Console;
+
+Clear();
+int size = 10;
+//var arr = size.CreateArray(min:1, max:10)
+size.CreateArray(min:1, max:10)
+            .Show()
+            .SortQuick(0, size - 1)
+            .Show()
+            ;
