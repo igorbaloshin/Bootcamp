13cf9c8c75968c4268f160d1c45715ae7afb9b5d
Igor B <iebaloshin@gmail.com>
2023-04-07 17:29:33 +0300

    буткемп 17

--- bootcamp016_OpenCV/video.py
+++ bootcamp016_OpenCV/video.py
@@ -2,29 +2,29 @@ import cv2
 
 face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 # картинка------------------------------------------------------------------------------
-img = cv2.imread('face.jpg')
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# img = cv2.imread('face.jpg')
+# img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascades.detectMultiScale(img_gray)
+# faces = face_cascades.detectMultiScale(img_gray)
 
-for (x, y, w, h) in faces:
-     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+# for (x, y, w, h) in faces:
+#      cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
 
-cv2.imshow('Result', img)
-cv2.waitKey(0)
+# cv2.imshow('Result', img)
+# cv2.waitKey(0)
 #видео вэбкамерв------------------------------------------------------------------------------
-# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-# while True:
-#     _, frame = cap.read()
-#     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     faces = face_cascades.detectMultiScale(img_gray)
+while True:
+    _, frame = cap.read()
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascades.detectMultiScale(img_gray)
 
-#     for (x, y, w, h) in faces:
-#         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-#     cv2.imshow('Result', frame)
+    cv2.imshow('Result', frame)
 
-#     if cv2.waitKey(1) & 0xff == ord('q'):
-#         break
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
